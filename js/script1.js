215770000d65b7c6994ada9c38ba5c14fc0f7a2f
Avik Roy <somavik99@gmail.com>
2023-10-16 00:55:01 +0530

    main

---
+++ js/script1.js
@@ -0,0 +1,8 @@
+(function () {
+  let MacBookMagnified = document.querySelector(".MacBook");
+  let MacBook = document.querySelector(".MacBook__image");
+  let MacBookDetails = document.querySelector(".WebD__Details");
+
+  MacBookMagnified.addEventListener("mouseenter",MagnifyImage,false);
+  
+})();
